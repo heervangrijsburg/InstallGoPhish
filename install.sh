fabe9b5c168cdc6aae0da8603a0d18a074768cfa
heervangrijsburg <31777075+heervangrijsburg@users.noreply.github.com>
2021-05-01 11:18:21 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,24 @@
+mkdir github
+versionWithHTML=$(curl -s https://github.com/gophish/gophish/releases/ | grep -m 1 linux-64bit.zip)
+versionWithHTML=${versionWithHTML//$'<td>'/}
+version=${versionWithHTML//$'</td>'/}
+
+#getGophishFile
+IFS="-"
+read -a versionArray <<< "$version"
+IFS=" "
+gophishfile=${versionArray[0]}-${versionArray[1]}-${versionArray[2]}
+if [ ! -d "$gophishfile" ]; then
+  mkdir $gophishfile
+  cd $gophishfile
+  wget https://github.com/gophish/gophish/releases/download/${versionArray[1]}/$version
+  unzip $version
+
+  #changePermissionsGophish
+  chmod +x gophish
+
+  #cleanUpFiles
+  rm -r $version
+else
+  echo file exits "$gophishfile"
+fi
